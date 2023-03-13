2d00300bda654eeb63d29de2c81548e9ddd1932a
Rafal Rudzinski <rafalrudzinski@icloud.com>
2023-03-12 20:14:12 -0600

    updated folder, scene names

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var sword_abillity: PackedScene
+@export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +12,7 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var sword_instance = sword_abillity.instantiate() as Node2D
+	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
 	sword_instance.global_position = player.global_position
 	
